5d494cd1d00dec2929520a341f414f62f70d2e75
yitaraZ <yarita.p44@gmail.com>
2024-03-16 20:59:28 +0700

    footer

--- SortingHat/src/component/Footer.jsx
+++ SortingHat/src/component/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="footer footer-center p-4 bg-base-300 text-base-content m-4">
-            <aside>
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content ">
+            <aside className="p-4">
                 <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
                 <p>This is Yayita pichayakulmongkol project</p>
                 <p>Thank you for visiting!</p>
